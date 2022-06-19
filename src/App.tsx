a51c17d884ec54d162f1f0a2178ab37025c3e3d7
guo133 <104018898+guo133@users.noreply.github.com>
2022-06-19 16:50:15 +0900

    complete!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useAllUsers } from "./hooks/useAllUsers";
+import { UserCard } from "./componets/UserCard";
 import "./styles.css";
 
 export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickfetchUser = () => getUsers();
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickfetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
